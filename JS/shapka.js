17a0cc31ed1363f238cdef9297c32090931e0110
Fedor <fedorkorepanov>
2019-01-27 20:34:13 +0300

    Add drop-down menu

--- JS/shapka.js
+++ JS/shapka.js
@@ -1,8 +1,10 @@
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 40) {
        $('#menu').addClass('fixed-menu');
+       $('header').addClass('fix-header');
     }
     else {
        $('#menu').removeClass('fixed-menu');
+       $('header').removeClass('fix-header');
     }
 });
