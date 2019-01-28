90e3a9bbe3329ae88ae90086873188bb2cc53e0a
unf0rgettable <42607380+unf0rgettable@users.noreply.github.com>
2019-01-28 10:29:37 +0300

    fix headers

--- JS/shapka.js
+++ JS/shapka.js
@@ -2,9 +2,12 @@ $(window).scroll(function(){
     if ($(window).scrollTop() >= 40) {
        $('#menu').addClass('fixed-menu');
        $('header').addClass('fix-header');
+       $('content').addClass('fixing-content');
+
     }
     else {
        $('#menu').removeClass('fixed-menu');
        $('header').removeClass('fix-header');
+       $('content').removeClass('fixing-content');
     }
 });
